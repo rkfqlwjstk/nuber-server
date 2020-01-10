98e897b86a4727d369921e0181efe00bfdf0254a
JuyoungKim <julong01@naver.com>
2020-01-10 17:01:32 +0900

    1.33 EmailSignIn Resolver part Two

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # nuber-server
 Server for the (N)Uber Clone Course on Nomad Academy. GraphQL, Typescript, NodeJS
+
+## Resolvers
+
+### Public Resolvers:
+
+- [x] Sign In / Sign Up with Facebook
+- [x] Sign In with Email
+- [ ] Start Phone Number Verification
+- [ ] Complete Phone Number Verification
+- [ ] Sign Up with Email
+
+---
+
+### Private Resolvers:
+
+- [ ] Generate JWT
+- [ ] Verify JWT
+- [ ] Verify Email
+- [ ] Get my Profile
+- [ ] Update my Profile
+- [ ] Toggle Driving Mode
+- [ ] Report Location / Orientation
+- [ ] Add Place
+- [ ] Edit Place
+- [ ] Delete Place
+- [ ] See Nearby Drivers
+- [ ] Subscribe to Nearby Drivers
+- [ ] Request a Ride
+- [ ] Get Nearby Ride Requests
+- [ ] Subscribe to Nearby Ride Requests
+- [ ] Subscribe to Ride Status
+- [ ] Get Chat Room Messages
+- [ ] Subscribe to Chat Room Messages
+- [ ] Send a Chat Message
+
+## Code Challenge
+
+- [ ] Get Ride History
+- [ ] See Ride Detail
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -24,9 +24,9 @@ const BCRYPT_ROUNDS = 10;
 class User extends BaseEntity {
   @PrimaryGeneratedColumn() id: number;
 
-  @Column({ type: "text", unique: true })
+  @Column({ type: "text", nullable: true })
   @IsEmail()
-  email: string;
+  email: string | null;
 
   @Column({ type: "boolean", default: false })
   verifiedEmail: boolean;
@@ -37,38 +37,41 @@ class User extends BaseEntity {
   @Column({ type: "text" })
   lastName: string;
 
-  @Column({ type: "int" })
+  @Column({ type: "int", nullable: true })
   age: number;
 
-  @Column({ type: "text" })
+  @Column({ type: "text", nullable: true })
   password: string;
 
-  @Column({ type: "text" })
+  @Column({ type: "text", nullable: true })
   phoneNumber: string;
 
   @Column({ type: "boolean", default: false })
   verifiedPhoneNumber: boolean;
-
+  
   @Column({ type: "text" })
   profilePhoto: string;
-
+  
   @Column({ type: "boolean", default: false })
   isDriving: boolean;
-
+  
   @Column({ type: "boolean", default: false })
   isRiding: boolean;
-
+  
   @Column({ type: "boolean", default: false })
   isTaken: boolean;
-
+  
   @Column({ type: "double precision", default: 0 })
   lastLng: number;
-
+  
   @Column({ type: "double precision", default: 0 })
   lastLat: number;
-
+  
   @Column({ type: "double precision", default: 0 })
   lastOrientation: number;
+  
+  @Column({ type: "text", nullable: true })
+  fbId: string;
 
   @ManyToOne(type => Chat, chat => chat.participants)
   chat: Chat;
